becb562f3864f9fa6ff0ed83d639f31041a40ffe
lNameless <ikasima0@gmail.com>
2026-01-29 12:59:48 +0100

    chore: minor fixes

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,10 +1,10 @@
-from pydantic import PostgresDsn
+from pydantic import PostgresDsn, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file="../.env", env_ignore_empty=True, extra="ignore"
+        env_file="../../.env", env_ignore_empty=True, extra="ignore"
     )
 
     POSTGRES_SERVER: str
